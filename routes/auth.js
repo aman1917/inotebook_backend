23a7846fc9dfb7f502681a29f895ef0b7b4a8d64
Aman Gupta <gupta.aman.1917@gmail.com>
2024-01-28 16:34:51 +0530

    password hash witj authtoken implementated

--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,11 @@
 const express = require("express");
 const User = require("../models/User");
+const bcrypt = require("bcryptjs");
 const { body, validationResult } = require("express-validator");
+const jwt = require("jsonwebtoken");
 const router = express.Router();
+const JWT_SCRETE = "AmanIsKing";
+
 //Create user using : POST "/api/auth".Create user
 
 router.post(
@@ -18,23 +22,36 @@ router.post(
       return res.status(400).json({ errors: result.array() });
     }
     //check weather the user with this email already exists
-    let user = await User.findOne({ email: req.body.email });
-    if (user) {
-      return res
-        .status(400)
-        .json({ errors: "Sorry this email already exists" });
+    try {
+      let user = await User.findOne({ email: req.body.email });
+      if (user) {
+        return res
+          .status(400)
+          .json({ errors: "Sorry this email already exists" });
+      }
+      const salt = await bcrypt.genSalt(10);
+      const secPass = await bcrypt.hash(req.body.password, salt);
+      user = await User.create({
+        name: req.body.name,
+        email: req.body.email,
+        password: secPass,
+      });
+
+      //generate jwt token with id
+      const data = {
+        user: {
+          id: user.id,
+        },
+      };
+
+      const authtoken = jwt.sign(data, JWT_SCRETE);
+      // console.log(jwtData);
+      // res.json({ meassage: "User Created" });
+      res.json({ authtoken });
+    } catch (error) {
+      console.log(error.meassage);
+      res.status(500).json("Some Error Occurs");
     }
-    user = await User.create({
-      name: req.body.name,
-      email: req.body.email,
-      password: req.body.password,
-    });
-    // .then((user) => res.json(req.body))
-    // .catch((err) => {
-    //   console.log(err);
-    //   res.json({ error: "Please Enter unique value for Email", message: err.message});
-    // });
-    res.json({ meassage: "User Created" });
   }
 );
 
